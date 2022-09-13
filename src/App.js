24140e3d1a9db7245bed383b3428a461db3ead87
EuronRamadanii <e.ramadani@digitbusiness.ch>
2022-09-13 10:53:44 +0200

    Added pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import './App.css';
-import Home from './components/pages/Home';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Services from './components/pages/Services';
-import Products from './components/pages/Products';
-import SignUp from './components/pages/SignUp';
+import React from "react";
+import Navbar from "./components/Navbar";
+import "./App.css";
+import Home from "./components/pages/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Services from "./components/pages/OurTeam";
+import Products from "./components/pages/Projects";
+import SignUp from "./components/pages/SignUp";
+import AboutUs from "./components/pages/AboutUs";
+import Projects from "./components/pages/Projects";
+import OurTeam from "./components/pages/OurTeam";
+import Partners from "./components/pages/Partners";
 
 function App() {
   return (
@@ -13,10 +17,12 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/services' component={Services} />
-          <Route path='/products' component={Products} />
-          <Route path='/sign-up' component={SignUp} />
+          <Route path="/" exact component={Home} />
+          <Route path="/aboutUs" component={AboutUs} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/ourTeam" component={OurTeam} />
+          <Route path="/partners" component={Partners} />
+          {/* <Route path="/sign-up" component={SignUp} /> */}
         </Switch>
       </Router>
     </>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { Button } from './Button';
-import { Link } from 'react-router-dom';
-import './Navbar.css';
+import React, { useState, useEffect } from "react";
+import { Button } from "./Button";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 function Navbar() {
   const [click, setClick] = useState(false);
@@ -22,55 +22,73 @@ function Navbar() {
     showButton();
   }, []);
 
-  window.addEventListener('resize', showButton);
+  window.addEventListener("resize", showButton);
 
   return (
     <>
-      <nav className='navbar'>
-        <div className='navbar-container'>
-          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
             Lorem Ipsum
-            <i class='fab fa-typo3' />
+            <i class="fab fa-typo3" />
           </Link>
-          <div className='menu-icon' onClick={handleClick}>
-            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
           </div>
-          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-            <li className='nav-item'>
-              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
                 Home
               </Link>
             </li>
-            <li className='nav-item'>
+            <li className="nav-item">
               <Link
-                to='/services'
-                className='nav-links'
+                to="/aboutUs"
+                className="nav-links"
                 onClick={closeMobileMenu}
               >
-                Services
+                About Us
               </Link>
             </li>
-            <li className='nav-item'>
+            <li className="nav-item">
               <Link
-                to='/products'
-                className='nav-links'
+                to="/projects"
+                className="nav-links"
                 onClick={closeMobileMenu}
               >
-                Products
+                Projects
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/ourTeam"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Our Team
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/partners"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Partners
               </Link>
             </li>
 
-            <li>
+            {/* <li>
               <Link
-                to='/sign-up'
-                className='nav-links-mobile'
+                to="/sign-up"
+                className="nav-links-mobile"
                 onClick={closeMobileMenu}
               >
                 Sign Up
               </Link>
-            </li>
+            </li> */}
           </ul>
-          {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+          {/* {button && <Button buttonStyle="btn--outline">SIGN UP</Button>} */}
         </div>
       </nav>
     </>
